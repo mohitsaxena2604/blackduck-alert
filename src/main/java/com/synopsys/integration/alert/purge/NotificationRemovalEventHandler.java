4c072ba2b4b8dfb9b767fd10f7a0d36329552020
psantos1113 <psantos@synopsys.com>
2021-09-22 07:31:07 -0400

    refactor: post an event to keep deleting.

--- src/main/java/com/synopsys/integration/alert/purge/NotificationRemovalEventHandler.java
+++ src/main/java/com/synopsys/integration/alert/purge/NotificationRemovalEventHandler.java
@@ -16,33 +16,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.synopsys.integration.alert.api.event.AlertEventHandler;
+import com.synopsys.integration.alert.api.event.EventManager;
 import com.synopsys.integration.alert.api.event.NotificationRemovalEvent;
 import com.synopsys.integration.alert.common.persistence.accessor.NotificationAccessor;
 
 @Component
 public class NotificationRemovalEventHandler implements AlertEventHandler<NotificationRemovalEvent> {
-    private static final int PAGE_SIZE = 50000;
+    private static final int PAGE_SIZE = 25000;
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     private NotificationAccessor notificationAccessor;
+    private EventManager eventManager;
 
     @Autowired
-    public NotificationRemovalEventHandler(NotificationAccessor notificationAccessor) {
+    public NotificationRemovalEventHandler(NotificationAccessor notificationAccessor, EventManager eventManager) {
         this.notificationAccessor = notificationAccessor;
+        this.eventManager = eventManager;
     }
 
     @Override
     public void handle(NotificationRemovalEvent event) {
         OffsetDateTime start = OffsetDateTime.now();
-        logger.debug("Event {}", event);
-        logger.info("Start notification removal event {}.", event.getEventId());
-        //TODO have this post another event to keep removing batches.
-        while (notificationAccessor.existsNotificationsToRemove()) {
-            notificationAccessor.deleteNotificationsForRemoval(PAGE_SIZE);
-        }
+        logger.debug("Start notification removal event {}.", event.getEventId());
+        notificationAccessor.deleteNotificationsForRemoval(PAGE_SIZE);
         OffsetDateTime end = OffsetDateTime.now();
         Duration duration = Duration.between(start, end);
-        logger.info("Notification removal event took {}", duration.toString());
-        logger.info("Finished notification removal event {}.", event.getEventId());
+        logger.debug("Notification removal event took {}", duration.toString());
+        if (notificationAccessor.existsNotificationsToRemove()) {
+            logger.debug("Additional notifications to remove detected. Posting new event.");
+            eventManager.sendEvent(new NotificationRemovalEvent());
+        }
+        logger.debug("Finished notification removal event {}.", event.getEventId());
     }
 }
